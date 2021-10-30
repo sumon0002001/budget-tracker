48b691b2fb338133e978485593264fd7139fa70a
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-10-31 01:51:02 +0600

    Add basic income components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Details from './components/Details/Details'
 
 const App = () => {
     return (
         <div>
-            App
+          < Details />
         </div>
     )
 }
